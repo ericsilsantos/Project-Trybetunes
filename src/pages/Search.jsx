05abec3ff8b0485dcdf27c1236b0e05e20362b89
Eric Santos <eric.silvasantos44@gmail.com>
2022-03-08 16:21:16 -0300

    Adicionado o quinto requisito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import Header from '../componets/Header';
 
+const LENGTH_MIN = 2;
+
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disabledButton: true,
+    };
+  }
+
+  handleButtonDisabled = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      disabledButton: (value.length < LENGTH_MIN),
+    });
+  }
+
   render() {
+    const { disabledButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <form action="">
+          <input
+            data-testid="search-artist-input"
+            onChange={ this.handleButtonDisabled }
+            type="text"
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ disabledButton }
+            type="button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
